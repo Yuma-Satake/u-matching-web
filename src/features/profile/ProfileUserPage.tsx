77505d068366b790e1f2a4140fa45576094d92d5
Yuma Satake <satous.y@icloud.com>
2025-01-16 10:41:31 +0900

    fix: 写真表示実装

--- src/features/profile/ProfileUserPage.tsx
+++ src/features/profile/ProfileUserPage.tsx
@@ -1,7 +1,7 @@
 import InstagramIcon from '@mui/icons-material/Instagram';
 import XIcon from '@mui/icons-material/X';
 import { HeaderLayout } from '@/components/layout/HeaderLayout';
-import { Avatar, Button, MenuItem, Select, Stack, TextField, Typography } from '@mui/material';
+import { Avatar, Box, Button, MenuItem, Select, Stack, TextField, Typography } from '@mui/material';
 import { FC, useEffect, useState } from 'react';
 import { Profile, UserWithIcon } from '@/types/supabase';
 import { supabaseClient } from '@/lib/supabaseClient';
@@ -11,6 +11,7 @@ import { supabaseClient } from '@/lib/supabaseClient';
 export const ProfileUserPage: FC = () => {
   const [user, setUser] = useState<UserWithIcon | null>(null);
   const [profile, setProfile] = useState<Profile | null>(null);
+  const [profilePhotos, setProfilePhotos] = useState<string[]>([]);
 
   useEffect(() => {
     const userId = window.location.pathname.split('/').pop();
@@ -29,6 +30,19 @@ export const ProfileUserPage: FC = () => {
 
       const photo = await supabaseClient.from('photos').select('*').eq('id', user.icon_img_id);
       if (!photo.data) return;
+
+      const photos = await supabaseClient.from('profile_photos').select('*').eq('id', userId);
+      if (!photos.data) return;
+
+      const photoIds = photos.data.map((photo) => photo.img_id);
+      const photoUrls = (await Promise.all(
+        photoIds.map(async (id) => {
+          const res = await supabaseClient.from('photos').select('img_url').eq('id', id);
+          return res.data?.[0]?.img_url;
+        })
+      ).then((urls) => urls.filter((url) => url))) as string[];
+
+      setProfilePhotos(photoUrls);
 
       setUser({ ...user, icon: photo.data[0].img_url });
       setProfile(profile);
@@ -50,6 +64,57 @@ export const ProfileUserPage: FC = () => {
         >
           <Avatar src={user?.icon ?? ''} sx={{ width: '65px', height: '65px', margin: 'auto' }} />
         </Stack>
+        {profilePhotos.length > 0 && (
+          <Stack
+            sx={{ pt: 1, pb: 1.5, width: '100%' }}
+            direction="row"
+            justifyContent="space-between"
+            alignItems="flex-end"
+          >
+            <Typography sx={{ fontWeight: 'bold' }}>写真</Typography>
+          </Stack>
+        )}
+        {profilePhotos.length > 0 && <Box height={'200px'} />}
+        <Stack
+          sx={{
+            width: '95dvw',
+            position: 'absolute',
+            top: '195px',
+            overflowX: 'auto',
+            pt: 0.5,
+          }}
+        >
+          <Stack direction="row" spacing={2}>
+            {profilePhotos.length > 0 &&
+              profilePhotos.map((photo, index) => (
+                <Stack
+                  key={`${photo}-${
+                    // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                    index
+                  }`}
+                  sx={{
+                    width: '200px',
+                    height: '200px',
+                    aspectRatio: '1/1',
+                  }}
+                >
+                  <Button sx={{ p: 0 }} disabled>
+                    <img
+                      alt="profile"
+                      src={photo}
+                      style={{
+                        height: '100%',
+                        width: '100%',
+                        borderRadius: '10px',
+                        aspectRatio: '1/1',
+                        objectFit: 'cover',
+                      }}
+                    />
+                  </Button>
+                </Stack>
+              ))}
+          </Stack>
+        </Stack>
         <Stack
           sx={{ pt: 1, pb: 1.5, width: '100%' }}
           direction="row"
@@ -58,7 +123,13 @@ export const ProfileUserPage: FC = () => {
         >
           <Typography sx={{ fontWeight: 'bold' }}>自己紹介</Typography>
         </Stack>
-        <TextField fullWidth multiline minRows={4} value={profile?.introduction ?? ''} />
+        <TextField
+          fullWidth
+          multiline
+          minRows={4}
+          value={profile?.introduction ?? ''}
+          sx={{ pointerEvents: 'none' }}
+        />
         <Typography sx={{ fontWeight: 'bold' }}>SNS</Typography>
         <Stack direction="row" spacing={2}>
           <Button
@@ -73,6 +144,10 @@ export const ProfileUserPage: FC = () => {
               px: 2,
               overflow: 'hidden',
             }}
+            onClick={() => {
+              if (!profile?.instagram) return;
+              window.open(`https://www.instagram.com/${profile.instagram}`);
+            }}
           >
             {profile?.instagram ?? '未設定'}
           </Button>
@@ -86,6 +161,10 @@ export const ProfileUserPage: FC = () => {
               borderWidth: '0.9px',
               px: 2,
               overflow: 'hidden',
+            }}
+            onClick={() => {
+              if (!profile?.twitter) return;
+              window.open(`https://twitter.com/${profile.twitter}`);
             }}
           >
             {profile?.twitter ?? '未設定'}
@@ -104,7 +183,13 @@ export const ProfileUserPage: FC = () => {
             ニックネーム
           </Typography>
           <Stack>
-            <TextField variant="outlined" fullWidth size="small" value={user?.nickname} />
+            <TextField
+              variant="outlined"
+              fullWidth
+              size="small"
+              value={user?.nickname}
+              sx={{ pointerEvents: 'none' }}
+            />
           </Stack>
         </Stack>
         <Stack spacing={0.5}>
@@ -112,7 +197,11 @@ export const ProfileUserPage: FC = () => {
             MBTI
           </Typography>
           <Stack>
-            <Select value={profile?.personal_types ?? '未選択'} size="small">
+            <Select
+              value={profile?.personal_types ?? '未選択'}
+              size="small"
+              sx={{ pointerEvents: 'none' }}
+            >
               {[
                 'ESTP（起業家）',
                 'ESFP（エンターテイナー）',
@@ -140,7 +229,10 @@ export const ProfileUserPage: FC = () => {
           </Stack>
         </Stack>
         <Stack spacing={0.5}>
-          <Typography variant="caption" sx={{ fontWeight: 'bold', color: 'gray' }}>
+          <Typography
+            variant="caption"
+            sx={{ fontWeight: 'bold', color: 'gray', pointerEvents: 'none' }}
+          >
             血液型
           </Typography>
           <Stack>
@@ -154,11 +246,18 @@ export const ProfileUserPage: FC = () => {
           </Stack>
         </Stack>
         <Stack spacing={0.5}>
-          <Typography variant="caption" sx={{ fontWeight: 'bold', color: 'gray' }}>
+          <Typography
+            variant="caption"
+            sx={{ fontWeight: 'bold', color: 'gray', pointerEvents: 'none' }}
+          >
             居住地
           </Typography>
           <Stack>
-            <Select value={profile?.address ?? '未選択'} size="small">
+            <Select
+              value={profile?.address ?? '未選択'}
+              size="small"
+              sx={{ pointerEvents: 'none' }}
+            >
               <MenuItem value={'岐阜県'}>岐阜県</MenuItem>
               <MenuItem value={'三重県'}>三重県</MenuItem>
               <MenuItem value={'愛知県'}>愛知県</MenuItem>
@@ -171,7 +270,13 @@ export const ProfileUserPage: FC = () => {
             学校
           </Typography>
           <Stack>
-            <TextField variant="outlined" fullWidth size="small" value={profile?.school ?? ''} />
+            <TextField
+              variant="outlined"
+              fullWidth
+              size="small"
+              value={profile?.school ?? ''}
+              sx={{ pointerEvents: 'none' }}
+            />
           </Stack>
         </Stack>
         <Stack spacing={0.5}>
@@ -179,7 +284,11 @@ export const ProfileUserPage: FC = () => {
             タバコ
           </Typography>
           <Stack>
-            <Select value={profile?.cigarettes ?? '未選択'} size="small">
+            <Select
+              value={profile?.cigarettes ?? '未選択'}
+              size="small"
+              sx={{ pointerEvents: 'none' }}
+            >
               <MenuItem value={'吸う'}>吸う</MenuItem>
               <MenuItem value={'吸わない'}>吸わない</MenuItem>
               <MenuItem value={'未選択'}>未選択</MenuItem>
@@ -190,7 +299,7 @@ export const ProfileUserPage: FC = () => {
           <Typography variant="caption" sx={{ fontWeight: 'bold', color: 'gray' }}>
             お酒
           </Typography>
-          <Select value={profile?.alcohol ?? '未選択'} size="small">
+          <Select value={profile?.alcohol ?? '未選択'} size="small" sx={{ pointerEvents: 'none' }}>
             <MenuItem value={'ときどき飲む'}>ときどき飲む</MenuItem>
             <MenuItem value={'飲む'}>飲む</MenuItem>
             <MenuItem value={'飲まない'}>飲まない</MenuItem>
